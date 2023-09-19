51af871f2bce53d721d6dbfa7204675b49d307f3
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-19 20:12:15 +0600

    Create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
 import TodosLogic from './components/TodoLogics'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className='todo-wrapper'>
+      <Navbar />
       <Header />
       <TodosLogic />
     </div>
